c79c85b4eca461fd61eb84335aad0cfe449bd8c3
James Dinsdale <hi@molovo.co>
2019-09-25 12:23:21 +0100

    Move company bundle page to settings menu

--- src/Superrb/KunstmaanCompanyBundle/Helper/Menu/CompanyMenuAdaptor.php
+++ src/Superrb/KunstmaanCompanyBundle/Helper/Menu/CompanyMenuAdaptor.php
@@ -5,23 +5,28 @@ namespace Superrb\KunstmaanCompanyBundle\Helper\Menu;
 use Kunstmaan\AdminBundle\Helper\Menu\MenuAdaptorInterface;
 use Kunstmaan\AdminBundle\Helper\Menu\MenuBuilder;
 use Kunstmaan\AdminBundle\Helper\Menu\MenuItem;
-use Kunstmaan\AdminBundle\Helper\Menu\TopMenuItem;
 use Symfony\Component\HttpFoundation\Request;
 
 class CompanyMenuAdaptor implements MenuAdaptorInterface
 {
+    /**
+     * @param MenuBuilder $menu
+     * @param array       $children
+     * @param MenuItem    $parent
+     * @param Request     $request
+     */
     public function adaptChildren(MenuBuilder $menu, array &$children, MenuItem $parent = null, Request $request = null)
     {
-        if (is_null($parent)) {
-            $menuItem = new TopMenuItem($menu);
+        if (!is_null($parent) && 'KunstmaanAdminBundle_settings' === $parent->getRoute()) {
+            $menuItem = new MenuItem($menu);
             $menuItem
                 ->setRoute('superrbkunstmaancompanybundle_admin_company')
                 ->setLabel('Company')
                 ->setUniqueId('Company')
-                ->setFolder(true)
                 ->setParent($parent);
             if (0 === stripos($request->attributes->get('_route'), $menuItem->getRoute())) {
                 $menuItem->setActive(true);
+                $parent->setActive(true);
             }
             $children[] = $menuItem;
         }
