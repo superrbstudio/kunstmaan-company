dde9f501d52e9578f0b5b23651a9695acbb3229c
James Dinsdale <hi@molovo.co>
2019-12-23 14:17:36 +0000

    Add missing use statements for input/output interfaces

--- src/Superrb/KunstmaanCompanyBundle/Command/MigrateCommand.php
+++ src/Superrb/KunstmaanCompanyBundle/Command/MigrateCommand.php
@@ -5,6 +5,8 @@ namespace Superrb\KunstmaanCompanyBundle\Command;
 use Doctrine\ORM\EntityManagerInterface;
 use ErrorException;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class MigrateCommand extends Command
 {
